3fa7ae6a45573545bb1b1216d826c30e218e4e37
EgeSaykan <shk.ync2005@gmail.com>
2025-11-01 21:29:09 +0000

    colours waa waa

--- Coord/find_checker.py
+++ Coord/find_checker.py
@@ -12,7 +12,6 @@ image_array = np.array(image)
 
 matrix_size = 47
 n = 50
-
 h, w, _ = image_array.shape
 # Create an output image initialized to black
 output_image = np.zeros((3, h, w, _), dtype=image_array.dtype)
@@ -32,7 +31,7 @@ for y in range(0, h, n):
             output_image[2, max(round(y-n*0.3), 0):min(round(y+n*1.3), h), max(round(x-n*0.3), 0):min(round(x+n*1.3), w)] = image_array[max(round(y-n*0.3), 0):min(round(y+n*1.3), h), max(round(x-n*0.3), 0):min(round(x+n*1.3), w)]
 
 gray_r = output_image[0,..., 0]  
-gray_g = output_image[1,..., 1]  
+gray_g = output_image[1,..., 0]  
 gray_b = output_image[2,..., 2]  
 
 combined_gray = np.stack([gray_r, gray_g, gray_b], axis=0)
@@ -43,54 +42,69 @@ combined_gray = np.stack([gray_r, gray_g, gray_b], axis=0)
 #     plt.axis('off')
 #     plt.title('Grayscale Output Image')
 #     plt.show()
-
-_, thresh_r = cv2.threshold(gray_r, 1, 255, cv2.THRESH_BINARY)
-_, thresh_g = cv2.threshold(gray_r, 1, 255, cv2.THRESH_BINARY)
-_, thresh_b = cv2.threshold(gray_r, 1, 255, cv2.THRESH_BINARY)
-
-# Find contours
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-if contours:
-    # Find the largest contour (assumed to be the checkered area)
-    largest_contour = max(contours, key=cv2.contourArea)
-    # Create a mask for the largest contour
-    mask = np.zeros_like(gray_r)
-    cv2.drawContours(mask, [largest_contour], -1, 255, -1)
-    # Get bounding rect of the largest contour
-    x, y, w, h = cv2.boundingRect(largest_contour)
-    x = x+round(w*0.25)
-    y = y+round(h*0.25)
-    w = round(w*0.5)
-    h *= round(h*0.5)
-    roi = output_image[y:y+h, x:x+w]
-    roi_mask = mask[y:y+h, x:x+w]
-    # Apply mask to ROI
-    roi_gray = roi[..., 0]  
-    plt.imshow(roi_gray, cmap='gray')
-    plt.axis('off')
-    plt.title('roi_gray')
-    plt.show()
-    roi_gray = cv2.bitwise_and(roi_gray, roi_mask)
-    plt.imshow(roi_gray, cmap='gray')
-    plt.axis('off')
-    plt.title('roi_gray22222')
-    plt.show()
-
-    # Define chessboard size (adjust as needed)
-    chessboard_size = (16, 16)
-    ret, corners = cv2.findChessboardCorners(roi_gray, chessboard_size, None)
-    while not ret:
-        chessboard_size = (chessboard_size[0]-1, chessboard_size[1]-1)
-        # Find chessboard corners in the ROI
-        ret, corners = cv2.findChessboardCorners(roi_gray, chessboard_size, None)
-
-    if ret:
-        # Draw chessboard corners on ROI
-        cv2.drawChessboardCorners(roi, chessboard_size, corners, ret)
-        # Draw blue border around the bounding box of the chessboard
-        cv2.rectangle(output_image, (x, y), (x+w, y+h), (255, 0, 0), 5)
-        plt.imshow(cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB))
+for i, arr in enumerate(combined_gray):
+    # _, thresh = cv2.threshold(arr, 1, 255, cv2.THRESH_BINARY)
+    gaus = cv2.GaussianBlur(arr, (5,5), 0)
+    contour, _ = cv2.findContours(gaus, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contour:
+        # Find the largest contour (assumed to be the checkered area)
+        largest_contour = max(contour, key=cv2.contourArea)
+        print(largest_contour.shape)
+        print(arr.shape)
+        # Create a mask for the largest contour
+        mask = np.zeros_like(arr)
+        cv2.drawContours(mask, [largest_contour], -1, 255, -1)
+        # Get bounding rect of the largest contour
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        x = x+round(w*0.25)
+        y = y+round(h*0.25)
+        w = round(w*0.5)
+        h *= round(h*0.5)
+        roi = output_image[i, y:y+h, x:x+w]
+        plt.imshow(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
         plt.axis('off')
-        plt.title('Chessboard in Largest Contour (Bordered Blue)')
+        plt.title('ROI')
         plt.show()
+        roi_mask = mask[y:y+h, x:x+w]
+        # Apply mask to ROI
+        roi_gray = roi[..., min(i+1, 2)]
+        print(roi_gray.shape)
+        # roi_gray = cv2.bitwise_and(roi_gray, roi_mask)
+        plt.imshow(roi_gray, cmap='gray')
+        plt.axis('off')
+        plt.title('roi_gray22222')
+        plt.show()
+        # plt.imshow(roi_gray, cmap='gray')
+        # plt.hist(roi_gray.ravel(), 256)
+        # plt.show()
+
+        # Define chessboard size (adjust as needed)
+        chessboard_size = (20, 20)
+        ret, corners = cv2.findChessboardCorners(roi_gray, chessboard_size, None)
+        while not ret:
+            if chessboard_size[1] < 7:
+                chessboard_size = (chessboard_size[0]-1, 20)
+            else:
+                chessboard_size = (chessboard_size[0], chessboard_size[1]-1)
+
+            print(chessboard_size)
+            # Find chessboard corners in the ROI
+            if (chessboard_size[0]) < 7:
+                break
+            ret, corners = cv2.findChessboardCorners(roi_gray, chessboard_size, None)
+        print(chessboard_size, end='\n\n')
+
+        if ret:
+            # Draw chessboard corners on ROI
+            print("roi shape:", roi.shape)
+            print("roi_gray shape:", roi_gray.shape)
+            print("corners shape:", corners.shape)
+            print("chessboard_size:", chessboard_size)
+            print("rows*cols:", chessboard_size[0]*chessboard_size[1])
+            cv2.drawChessboardCorners(roi, chessboard_size, corners, ret)
+            # Draw blue border around the bounding box of the chessboard
+            cv2.rectangle(output_image[i], (x, y), (x+w, y+h), (255, 0, 0), 5)
+            plt.imshow(cv2.cvtColor(output_image[i], cv2.COLOR_BGR2RGB))
+            plt.axis('off')
+            plt.title('Chessboard in Largest Contour (Bordered Blue)')
+            plt.show()
